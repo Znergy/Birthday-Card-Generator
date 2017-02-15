b3cc70c5a8e28b9485ba28c4b5ba1581d79fd925
Grace Stuart and Ryan Jones <grace@>
2017-02-15 15:25:43 -0800

    add comments of what needs to be done in js.

---
+++ js/bdCard.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  // checkbox
+
+  // to email, from email, name
+
+  // custom message
+
+  // color wheel
+
+  // date picker
+
+  // page radius
+
+  // submit button
+
+
+
+
+
+
+  // stop refresh
+  event.preventDefault();
+});
